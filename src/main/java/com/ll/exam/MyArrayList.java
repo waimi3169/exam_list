ccfa0cbcfb70ebd598c539554eae43e7812a5f46
깃waimi3169 <waimi3169@naver.com>
2023-04-19 14:32:35 +0900

    size, add

---
+++ src/main/java/com/ll/exam/MyArrayList.java
@@ -0,0 +1,13 @@
+package com.ll.exam;
+
+public class MyArrayList<T> {
+    private int size = 0;
+    public int size() {
+        return 0;
+    }
+
+    public boolean add(T element) {
+        size++;
+        return true;
+    }
+}
---
+++ src/test/java/com/ll/exam/MyArrayListTest.java
@@ -0,0 +1,26 @@
+package com.ll.exam;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.ll.exam.MyArrayList;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+public class MyArrayListTest {
+    @Test
+    void t1() {
+        MyArrayList<String> list = new MyArrayList<>();
+        assertThat(list.size()).isEqualTo(0);
+    }
+
+    @Test
+    void t2() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과");
+        list.add("포도");
+
+        assertThat(list.size()).isEqualTo(2);
+    }
+}
