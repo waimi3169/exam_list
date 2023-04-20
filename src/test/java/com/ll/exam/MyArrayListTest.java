15b442f152a3918831fb75f597c81d04c9753e37
깃waimi3169 <waimi3169@naver.com>
2023-04-20 09:06:25 +0900

    TestUt구현

--- src/test/java/com/ll/exam/MyArrayListTest.java
+++ src/test/java/com/ll/exam/MyArrayListTest.java
@@ -1,20 +1,21 @@
 package com.ll.exam;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.ll.TestUt;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import com.ll.exam.MyArrayList;
+import static org.assertj.core.api.Assertions.assertThat;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 public class MyArrayListTest {
     @Test
+    @DisplayName("size()")
     void t1() {
         MyArrayList<String> list = new MyArrayList<>();
         assertThat(list.size()).isEqualTo(0);
     }
 
     @Test
+    @DisplayName("add(\"사과\")")
     void t2() {
         MyArrayList<String> list = new MyArrayList<>();
 
@@ -25,13 +26,36 @@ public class MyArrayListTest {
     }
 
     @Test
+    @DisplayName("get(1)")
     void t3() {
         MyArrayList<String> list = new MyArrayList<>();
+
+        list.add("사과"); // data[0];
+        list.add("포도"); // data[1];
+
+        assertThat(list.get(0)).isEqualTo("사과"); // data[0];
+        assertThat(list.get(1)).isEqualTo("포도"); // data[1];
+    }
+
+    @Test
+    @DisplayName("data(배열)의 크기가 자동으로 늘어나야 한다.")
+    void t4() {
+        MyArrayList<String> list = new MyArrayList<>();
+
+        String[] data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(2);
 
         list.add("사과");
         list.add("포도");
 
-        assertThat(list.get(1)).isEqualTo("포도");
+        data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(2);
 
+        list.add("당근");
+
+        data = TestUt.getFieldValue(list, "data", null);
+        assertThat(data.length).isEqualTo(4);
+
+        assertThat(list.size()).isEqualTo(3);
     }
 }
